414afba68c493641ae2b715490349e64af49be34
Jimmy <Jimmy0319@gmail.com>
2020-12-22 16:47:06 +0800

    Fix subnet name

--- main.tf
+++ main.tf
@@ -50,6 +50,6 @@ resource "azurerm_route_table" "example" {
 }
 
 resource "azurerm_subnet_route_table_association" "default" {
-  subnet_id      = azurerm_subnet.default.id
+  subnet_id      = azurerm_virtual_network.default.id
   route_table_id = azurerm_route_table.example.id
 }
