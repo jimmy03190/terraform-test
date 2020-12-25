49b517d614b33d652730787c63378c6a2f73d5a5
Jimmy <Jimmy0319@gmail.com>
2020-12-25 16:53:44 +0800

    Change ID to app ID

--- main.tf
+++ main.tf
@@ -62,5 +62,5 @@ data "azurerm_client_config" "example" {
 resource "azurerm_role_assignment" "example" {
   scope                = azurerm_virtual_network.example.id
   role_definition_id   = "4d97b98b-1d4f-4787-a291-c67834d212e7"
-  principal_id         = "55768254-98fb-4052-b874-4413bd25608f"
+  principal_id         = "20151c08-b703-45dd-acda-7efaac72cb87"
 }
